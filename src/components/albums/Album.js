91cfe786255d2e2fead214e8622428fc8bc4a2d7
Sally Erisman <sally.erisman@gmail.com>
2021-01-06 22:56:08 +0100

    Enabled editing of album title (for realz this time...).

--- src/components/albums/Album.js
+++ src/components/albums/Album.js
@@ -1,22 +1,34 @@
+import { useState } from 'react';
 import { useParams } from 'react-router-dom'
 import useAlbum from '../../hooks/useAlbum'
+import EditTitle from './EditTitle'
 import ImageGrid from './ImageGrid'
 import UploadImage from './UploadImage'
 
 const Album = () => {
 	const { albumId } = useParams()
 	const { album, loading } = useAlbum(albumId)
+	const [editTitle, setEditTitle] = useState(false);
+
+	const handleEditTitle = () => {
+        setEditTitle(true);
+    };
 	
 	return (
-		<>
-			<h2>{album && album.title}</h2>
-
-			<UploadImage albumId={albumId} />
-
+		<>	
 			{loading
 				? <div>Loading...</div>
 				: album && 
-					<ImageGrid images={album.images} />
+					<>
+						{editTitle 
+							? <EditTitle album={album}/> 
+							: <>
+								<h2>{album.title} <span onClick={handleEditTitle}>🖋</span></h2>
+								<UploadImage albumId={albumId} />
+								<ImageGrid images={album.images} />
+							</>
+						}
+					</>
 			}
 		</>
 	)
