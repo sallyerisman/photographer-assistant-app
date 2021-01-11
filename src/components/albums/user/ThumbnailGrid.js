f8cf24f4a775bcfb8c5c310509459788d7ca8896
Sally Erisman <sally.erisman@gmail.com>
2021-01-11 17:21:04 +0100

    Moved thumbnail grid into a seperate component.

---
+++ src/components/albums/user/ThumbnailGrid.js
@@ -0,0 +1,79 @@
+import { useEffect, useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+import { Alert, Button, Image, Col, Row } from 'react-bootstrap'
+import { ArrowCounterclockwise } from 'react-bootstrap-icons'
+import { SRLWrapper } from 'simple-react-lightbox'
+import { useRateImageContext } from '../../../contexts/RateImageContext'
+import useApproveImages from '../../../hooks/useApproveImages'
+
+const ThumbnailGrid = ({ images, owner, title }) => {
+	const [approvedImages, setApprovedImages] = useState(null)
+	const [errorMessage, setErrorMessage] = useState(false)
+
+	const navigate = useNavigate()
+	const { reviewError, reviewSuccess } = useApproveImages(approvedImages, owner, title)
+	const { dislikedImages, handleDislike, handleLike, likedImages  } = useRateImageContext()
+
+	useEffect(() => {
+		if (reviewError) {
+			setErrorMessage("An error occurred and your selection could not be saved.")
+		} else if (reviewSuccess) {
+			// Prevent duplicate upload
+			setApprovedImages(null)
+			navigate('/thank-you')
+		} 
+	}, [reviewError, reviewSuccess]);
+
+	const handleFinalizeSelection = (likedImages) => {
+		setApprovedImages(likedImages)
+	}
+
+	return (
+		<SRLWrapper>
+			{errorMessage && <Alert variant="danger">{errorMessage}</Alert>}
+			{images && likedImages.length > 0 || dislikedImages.length > 0 &&
+				<Alert variant="info" className="alert__count">{`You have approved ${likedImages.length}/${images.length} images`}</Alert>
+			}
+
+			<>
+				{likedImages.length > 0 &&
+					<>
+						<h2>Images to keep:</h2>
+						<Row className="thumbnail-grid">
+							{likedImages.map((image, index) => (
+								<Col xs={4} md={3} lg={2} key={index}>
+									<div className="thumbnail-wrapper">
+										<Image className="thumnail" src={image.url} alt={image.name} fluid thumbnail/>
+										<ArrowCounterclockwise className="icon icon__undo" onClick={() => handleDislike(image)} />
+									</div>
+								</Col>
+							))}
+						</Row>
+					</>
+				}							
+					
+				{dislikedImages.length > 0 &&
+					<>
+						<h2>Images to remove:</h2>
+						<Row className="thumbnail-grid">
+							{dislikedImages.map((image, index) => (
+								<Col xs={4} md={3} lg={2} key={index}>
+									<div className="thumbnail-wrapper">
+										<Image src={image.url} alt={image.name} fluid thumbnail/>
+										<ArrowCounterclockwise className="icon icon__undo" onClick={() => handleLike(image)} />	
+									</div>
+								</Col>
+							))}
+						</Row>
+					</>
+				}	
+			
+				<div>
+					<Button className="btn button__primary" onClick={() => handleFinalizeSelection(likedImages)}>Finalize your selection</Button>
+				</div>
+			</>				
+		</SRLWrapper>
+	)
+}
+
+export default ThumbnailGrid
